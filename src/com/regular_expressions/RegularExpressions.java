252f51a327f5db17f774d738cda9647ec3347ad8
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-21 18:59:22 +0530

    Added : Should have at least one Upper case letter in password

--- src/com/regular_expressions/RegularExpressions.java
+++ src/com/regular_expressions/RegularExpressions.java
@@ -56,19 +56,31 @@ public class RegularExpressions {
   public static void password(){
       Pattern pattern = Pattern.compile("^\\w{8,}$");
 
-      Matcher matcher = pattern.matcher("sdflkjdshkewr23423lk4");
+      Matcher matcher = pattern.matcher("abc12345");
       if (matcher.find() == true) {
           System.out.println("Password is Valid");
       }else{
           System.out.println("Password is Invalid");
       }
   }
+ // Uc6 =  Rule2– Should have at least 1 Upper Case
+ public static void passwordRule2(){
+     Pattern pattern = Pattern.compile("^(?=.*[A-Z])[\\w]{8,}$");
+
+     Matcher matcher = pattern.matcher("Abcd12345");
+     if (matcher.find() == true) {
+         System.out.println("Password is Valid");
+     }else{
+         System.out.println("Password is Invalid");
+     }
+ }
   public static void main(String[] args) {
       firstName();
       lastName();
       validEmail();
       phoneNumber();
       password();
+      passwordRule2();
   }
 }
 
